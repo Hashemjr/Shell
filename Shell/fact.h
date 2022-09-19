34a12233ec1ed4c5c4a91129df759d97aad96ec6
Marwan Hashim <67843870+Hashemjr@users.noreply.github.com>
2022-09-19 21:29:40 +0200

    Add files via upload

---
+++ Shell/fact.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void Factorial(){
+	
+	int number , sum = 1;
+
+	printf("\nEnter a number: ");
+	scanf("%d",&number);
+	getchar();
+
+	for(int i=1; i<=number; i++){
+		sum *= i;
+	}
+
+	printf("Result is: %d\n",sum);
+
+}
---
+++ Shell/shell2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "rand.h"
+#include "fact.h"
+#include "fibb.h"
+
+int main()
+{
+	char prompt [] = "Let's hack> ";
+	char* sentence = NULL ;
+	size_t buffer = 100;
+	sentence = (char*) malloc(100 * sizeof(char));
+
+	printf("$ ./myFemtoShell\n\n");
+
+	while(1){
+
+		printf("%s",prompt);
+		scanf("%[^\n]%*c" , sentence);
+
+		if(strcmp(sentence, "exit") == 0){
+			printf("Good Bye :)\n");
+			printf("$\n");
+			exit(0);
+		}
+
+		else if(strcmp(sentence , "rand") == 0){
+			Rando();
+			continue;
+		}
+
+		else if(strcmp(sentence , "fact") == 0){
+			Factorial();
+			continue;
+		}
+
+		else if(strcmp(sentence, "fib") == 0){
+			Fibonacci();
+			continue;
+		}
+
+		printf("\nYou said: %s\n", sentence);
+	}
+
+	free(sentence);
+	return 0;
+}
